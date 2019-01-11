cef63a5c2e38bcb4d492dd1e87efa01cf0b8cfa8
higashi000 <higashi.ict@gmail.com>
2019-01-11 11:49:07 +0900

    [add] new install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+`mv .fishrc ~/`
+
+echo -n "Are you need proxy settings?(y/n): "
+read _needProxySettings
+case $_needProxySettings in
+  [y] | '' )
+    echo -n "Please input your proxy adress: "
+    read _yourProxy
+    echo -n "Please input your SSID: "
+    read _yourSSID
+    echo "set _proxyAdress $_yourProxy" >> ~/.proxySetting
+    echo "set _SSID $_yourSSID" >> ~/.proxySetting
+    `cp .switch_proxy ~/`
+    break;;
+
+  [n] )
+    break;;
+esac
+
+echo -n "Are you need Neovim and dein settings?(y/n): "
+read _needNeovimSettings
+case $_needNeovimSettings in
+  [y] | '' )
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+    sh ./installer.sh ~/.cache/dein
+    break;;
+  [n])
+    break;;
+esac
+
+`mv nvim ~/.config/`
